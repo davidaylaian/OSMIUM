67107e1b71dbf0a129de4f2bbe1bf26a74b9e9f9
David Aylaian <davidaylaian@protonmail.com>
2018-01-20 23:51:41 -0500

    Add a few basic headers
    stdarg.h, stdbool.h, stddef.h, and stdint.h

---
+++ kernel/include/stdbool.h
@@ -0,0 +1,16 @@
+/**
+ *
+ * Copyright 2018 David Aylaian
+ * https://github.com/davidaylaian/OSMIUM/
+ * Licensed under the Apache License 2.0
+ *
+ */
+
+#ifndef _STDBOOL_H
+#define _STDBOOL_H
+
+#define bool	_Bool
+#define true	1
+#define false	0
+
+#endif
